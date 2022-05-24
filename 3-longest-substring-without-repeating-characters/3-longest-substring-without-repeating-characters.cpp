db06b6b11e244e4821fd07f519763d8268592083
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-05-24 15:44:00 +0200

    Time: 3 ms (98.52%), Space: 8.3 MB (58.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,38 +2,29 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
    
-        
-   vector<pair<int , int >> wordFreqWithLastIndex(266);
+   
+    vector<int> lastVisitedIndex(266 , -1);
 	int start, end;
 	start = end = 0;
 	int maxSize = 0;
 	string word = s;
-
 	while (end < word.size())
 	{
-		int charIndex = word[end];
-		if (wordFreqWithLastIndex[charIndex].first == 0)
+		if (lastVisitedIndex[word[end]] != -1)
 		{
-			wordFreqWithLastIndex[charIndex].first = 1;
-			wordFreqWithLastIndex[charIndex].second = end;
-			maxSize = max(maxSize,end-start+1);
-			end++;
-		}
-		else
-		{
-			int charLastIndex = wordFreqWithLastIndex[charIndex].second + 1;
-			for (int counter = start; counter <= wordFreqWithLastIndex[charIndex].second; counter++)
+			int nextStart = lastVisitedIndex[word[end]] + 1;
+			for (; start < nextStart; start++)
 			{
-				wordFreqWithLastIndex[word[counter]].first = 0;
-				wordFreqWithLastIndex[word[counter]].second = 0;
+				lastVisitedIndex[word[start]] = -1;
 			}
-			start = charLastIndex;
 		}
-
+		lastVisitedIndex[word[end]] = end;
+		maxSize = max(maxSize, end - start + 1);
+		end++;
 
 	}
-
 	return maxSize;
+   
         
     }
 };
