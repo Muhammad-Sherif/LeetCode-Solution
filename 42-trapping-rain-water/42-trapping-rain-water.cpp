827224b10b54e0b22a68058f084fd2325d096b5e
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-05-28 17:40:41 +0200

    Time: 20 ms (45.27%), Space: 20.7 MB (5.99%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-	int n = (int) height.size();
+   vector<int> SuffixMax(vector<int>& height) {
+	int n = (int)height.size();
 	vector<int> suffixMax(n);
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (i == n - 1)
+			suffixMax[i] = height[i];
+		else
+			suffixMax[i] = max(height[i], suffixMax[i + 1]);
+	}
+	return suffixMax;
+
+}
+
+
+vector<int> PrefixMax(vector<int>& height) {
+	int n = (int)height.size();
 	vector<int> prefixMax(n);
-	int waterCount = 0;
 	for (int i = 0; i < n; i++)
 	{
 		if (!i)
 			prefixMax[i] = height[i];
 		else
-			prefixMax[i] = max(height[i] , prefixMax[i-1]);
+			prefixMax[i] = max(height[i], prefixMax[i - 1]);
 	}
+	return prefixMax;
+}
 
-	for (int i = n - 1; i >= 0; i--)
-	{
-		if(i == n-1)
-			suffixMax[i] = height[i];
-		else
-			suffixMax[i] = max(height[i], suffixMax[i + 1]);
-	}
+int trap(vector<int>& height) {
 
+	int n = height.size();
+	int waterCount = 0;
+	vector<int> prefixMax, suffixMax;
+	prefixMax = PrefixMax(height);
+	suffixMax = SuffixMax(height);
 	for (int i = 1; i < n-1; i++)
 	{
 		if (prefixMax[i - 1] > height[i] && suffixMax[i + 1] > height[i])
